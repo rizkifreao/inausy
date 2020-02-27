e7d9f87d2b2419cdc62b0bf0a086aa3e1c7743a4
Rizki Pebrianto <rizkipebrianto96@gmail.com>
2020-02-27 11:21:30 +0700

    inital commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?
+// Ambil User Agent dari config sc Mastercoin atau indigocoin atau bitcoinbep
+// Atau Capture dulu ambil User Agentnya agar system tidak mendetek penyalahgunaan
+
+$UserAgent = "Mozilla/5.0 (Linux; Android 9.0.0; MI 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.93 Mobile Safari/537.36";
+
+// Masukan email dan password akun agan
+
+$Email = "xxxxxxxxx";
+$Password = "xxxxxxxx";
